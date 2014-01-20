6851e6bed5b8743afea3c73b8af4e8b4eb7fc32a
cris8259 <huangjian8000@126.com>
2014-01-20 17:16:25 +0800

    Update PID_v1.h

--- PID_v1/PID_v1.h
+++ PID_v1/PID_v1.h
@@ -2,6 +2,37 @@
 #define PID_v1_h
 #define LIBRARY_VERSION	1.0.0
 
+//Constants used in some of the functions below
+#define AUTOMATIC    1
+#define MANUAL	     0
+#define DIRECT       0
+#define REVERSE      1
+
+typedef struct
+{
+	float dispKp;				// * we'll hold on to the tuning parameters in user-entered 
+	float dispKi;				//   format for display purposes
+	float dispKd;				//
+    
+	float kp;                  // * (P)roportional Tuning Parameter
+	float ki;                  // * (I)ntegral Tuning Parameter
+	float kd;                  // * (D)erivative Tuning Parameter
+
+	int controllerDirection;
+
+	float *myInput;              // * Pointers to the Input, Output, and Setpoint variables
+	float *myOutput;             //   This creates a hard link between the variables and the 
+	float *mySetpoint;           //   PID, freeing the user from having to constantly tell us
+                                  //   what these values are.  with pointers we'll just know.
+			  
+	unsigned long lastTime;
+	float ITerm, lastInput;
+
+	unsigned long SampleTime;
+	float outMin, outMax;
+	char inAuto;
+}PID_T;
+
 class PID
 {
 
