bd9d952b07c21fe296edd98671ff2ffa2f0b151d
cris8259 <huangjian8000@126.com>
2014-01-22 10:26:07 +0800

    Update PID_v1.h

--- PID_v1/PID_v1.h
+++ PID_v1/PID_v1.h
@@ -42,6 +42,18 @@ void PID_SetOutputLimits(PID_T *pid, float Min, float Max);
 void PID_SetMode(PID_T *pid, int Mode);
 void PID_SetControllerDirection(PID_T *pid, int Direction);
 
+/* Status Funcions*************************************************************
+ * Just because you set the Kp=-1 doesn't mean it actually happened.  these
+ * functions query the internal state of the PID.  they're here for display 
+ * purposes.  this are the functions the PID Front-end uses for example
+ ******************************************************************************/
+float PID_GetKp(PID_T *pid);
+float PID_GetKi(PID_T *pid);
+float PID_GetKd(PID_T *pid);
+
+int PID_GetMode(PID_T *pid);
+int PID_GetDirection(PID_T *pid);
+
 
 #endif
 
